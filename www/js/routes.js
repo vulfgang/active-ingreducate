9f83c13b12990cf6c3ea8e5f0129adcf44180415
Tuan Pham <tt.pham1991+git@gmail.com>
2015-10-24 03:18:43 -0700

    Changed starting template.

---
+++ www/js/routes.js
@@ -0,0 +1,45 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('index', {
+      url: '/',
+      templateUrl: 'templates/index.html',
+      controller: 'indexCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('capture', {
+      url: '/capture',
+      templateUrl: 'templates/capture.html',
+      controller: 'captureCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('history', {
+      url: '/history',
+      templateUrl: 'templates/history.html',
+      controller: 'historyCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+
+});
