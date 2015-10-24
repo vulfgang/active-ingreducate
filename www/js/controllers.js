c4ff7b6c3e735eb6488837b982c8a9dbe55ec2d3
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-10-24 09:07:59 -0700

    Push unfinished capture code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,40 @@
 angular.module('app.controllers', [])
    
-.controller('captureCtrl', function($scope) {
+.controller('captureCtrl', ['$scope', 'Camera', function($scope, Camera) {
+  $scope.getPhoto = function() {
+    $scope.lastPhoto = '/img/sample.jpg';
+    $scope.ocrString = 'doge';
 
-})
-   
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var img = document.createElement('IMG');
+    img.src = '/img/sample.jpg';
+    img.onload = function() {
+      document.write('hi');
+      context.drawImage(img, 0, 0 );
+      var myData = context.getImageData(0, 0, img.width, img.height);
+      $scope.ocrString = OCRAD(myData);
+      if ($scope.ocrString.length !== 0) {
+        $scope.ocrString = 'doge';
+      }
+    };
+
+    // Camera.getPicture().then(function(imageURI) {
+    //   console.log(imageURI);
+    //   $scope.lastPhoto = imageURI;
+    //   var ocrString = OCRAD(imageURI);
+    // }, function(err) {
+    //   console.err(err);
+    // }, {
+    //   quality: 75,
+    //   targetWidth: 320,
+    //   targetHeight: 320,
+    //   saveToPhotoAlbum: false,
+    //   correctOrientation: true
+    // });
+  };
+}])
+
 .controller('historyCtrl', function($scope) {
 
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,31 @@
 angular.module('app.services', [])
 
 .service('OCRService', ['CaptureService', function(image){
-  var string = OCRAD(image);
-  this.getText = function() {
-    return string;
+  return {
+    getText: function() {
+      return OCRAD(image);
+    }
+  };
+  // var string = OCRAD(image);
+  // this.getText = function() {
+  //   return string;
+  // };
+}])
+
+.factory('Camera', ['$q', function($q) {
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
   };
 }])
 
