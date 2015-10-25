538968bed215743e9c95d2cb63ef6f0369525aea
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-10-24 21:40:09 -0700

    Clean up and "complete" code for captureCtrl (untested)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,39 +1,50 @@
 angular.module('app.controllers', [])
    
-.controller('captureCtrl', ['$scope', 'Camera', 'ParserService', function($scope, Camera, ParserService) {
+.controller('captureCtrl', function($scope, Camera, $cordovaFileTransfer) {
   $scope.getPhoto = function() {
+    Camera.getPicture().then(function(imageURI) {
+      console.log(imageURI);
+      $scope.lastPhoto = imageURI;
+    }, function(err) {
+      console.err(err);
+    }, {
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    });
 
-    $scope.lastPhoto = '/img/sample.jpg';
-    $scope.ocrString = 'de';
-
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
-    var img = document.createElement('IMG');
-    img.src = '/img/sample.jpg';
-    img.onload = function() {
-      context.drawImage(img, 0, 0, img.width, img.height);
-      context.getImageData(0, 0, img.width, img.height);
-      var text = OCRAD(canvas);
-      var parsed = ParserService.getParsed(text);
-      document.getElementById("ocrString").innerHTML = text;
-      document.getElementById("parsed").innerHTML = parsed;
-    };
+    return $scope.lastPhoto;
   };
 
-    // Camera.getPicture().then(function(imageURI) {
-    //   console.log(imageURI);
-    //   $scope.lastPhoto = imageURI;
-    //   var ocrString = OCRAD(imageURI);
-    // }, function(err) {
-    //   console.err(err);
-    // }, {
-    //   quality: 75,
-    //   targetWidth: 320,
-    //   targetHeight: 320,
-    //   saveToPhotoAlbum: false,
-    //   correctOrientation: true
-    // });
-}])
+  $scope.parsePhoto = function(imageURI) {
+    var ocrString;
+
+    var options = {
+      fileName: 'image.jpg',
+      chunkedMode: false,
+      mimeType: 'image/jpg'
+    };
+    $cordovaFileTransfer.upload("http://192.168.43.96/ocr", imageURI, options).then(function(result) {
+      ocrString = JSON.stringify(result.response);
+      console.log("SUCCESS: " + ocrString);
+    }, function(err) {
+      ocrString = JSON.stringify(err);
+      console.log("ERROR: " + ocrString);
+    }, function (progress) {
+      ocrString = JSON.stringify(progress);
+      console.log("PROGRESS: " + ocrString);
+    });
+
+    return ocrString;
+  };
+
+  $scope.getAndParsePhoto = function() {
+    var imageURI = $scope.getPhoto(); // also sets $scope.lastPhoto
+    $scope.parsedString = $scope.parsePhoto(imageURI);
+  };
+})
 
 .controller('historyCtrl', function($scope) {
 
